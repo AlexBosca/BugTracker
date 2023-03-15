d6bbbbde907411dc764c9af59ec96caa798acda3
AlexBosca <alex_241998@yahoo.com>
2023-03-15 05:34:56 +0200

    frontend - created basic authentication interceptor

---
+++ frontend/src/app/core/interceptor/basic-auth.interceptor.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpHeaders
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/features/feature-auth/services/auth.service';
+
+@Injectable()
+export class BasicAuthInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const currentUser = this.authService.getCurrentUser();
+    const token = this.authService.getCurrentUserAuthToken();
+
+    if(currentUser && token) {
+        request = request.clone({
+          setHeaders: {
+            Authorization: token
+          }
+        });
+    }
+
+    return next.handle(request);
+  }
+}
