659ab85572f09a07692674a34beea36738f71d8c
AlexBosca <alex_241998@yahoo.com>
2023-03-15 06:39:52 +0200

    frontend - added routing to other modules

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   {
     path: 'issues',
-    loadChildren: () => import('./features/feature-issues/feature-issues.module').then(m => m.FeatureIssuesModule)
+    loadChildren: () => import('./features/feature-issues/feature-issues.module').then(m => m.FeatureIssuesModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'teams',
+    loadChildren: () => import('./features/feature-teams/feature-teams.module').then(m => m.FeatureTeamsModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'projects',
+    loadChildren: () => import('./features/feature-projects/feature-projects.module').then(m => m.FeatureProjectsModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./features/feature-auth/feature-auth.module').then(m => m.FeatureAuthModule)
   },
   {
     path: '',
