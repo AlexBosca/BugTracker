297a2764fbcde66a3d8cd07a095ad3152a9d14bf
AlexBosca <alex_241998@yahoo.com>
2023-03-15 06:29:45 +0200

    frontend - implemented issue service functionality

--- frontend/src/app/features/feature-issues/services/issue.service.ts
+++ frontend/src/app/features/feature-issues/services/issue.service.ts
@@ -1,93 +1,70 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { catchError, Observable, of, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { AuthService } from '../../feature-auth/services/auth.service';
 import { IssueModel } from '../models/IssueModel';
+import { IssueRequestModel } from '../models/IssueRequestModel';
+import { IssueStatusRequest } from '../models/IssueStatusRequest';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IssueService {
-  issues: IssueModel[] = [
-    {
-      'createdBy': 'Ionut Predeal',
-      'issueId': 'PPDS0001',
-      'title': 'Some Issue Title',
-      'version': '1.200.2',
-      'asignee': 'Dorel Bolovaneanu',
-      'priority': 'HIGH',
-      'status': 'ASSIGNED',
-      'createdOn': '21 Feb 2022',
-      'description': 'Dummy Description',
-      'reproducingSteps': 'Steps to reproduce the issue',
-      'asignedOn': '21 Feb 2022',
-      'environment': 'Some Environment',
-      'closedBy': 'Ionut Predeal',
-      'closedOn': '12 Mar 2022',
-      'tester': 'Bogdan Popescu',
-      'discussion': ['comment', 'another comment'],
-      'project': 'Project'
-    },
-    {
-      'createdBy': 'Bogdan Popescu',
-      'issueId': 'PPDS0002',
-      'title': 'Other Issue Title',
-      'version': '1.200.2',
-      'asignee': 'Dorel Bolovaneanu',
-      'priority': 'HIGHEST',
-      'status': 'NEW',
-      'createdOn': '21 Feb 2022',
-      'description': 'Dummy Description',
-      'reproducingSteps': 'Steps to reproduce the issue',
-      'asignedOn': '21 Feb 2022',
-      'environment': 'Some Environment',
-      'closedBy': 'Ionut Predeal',
-      'closedOn': '12 Mar 2022',
-      'tester': 'Bogdan Popescu',
-      'discussion': ['comment', 'another comment'],
-      'project': 'Project'
-    },
-    {
-      'createdBy': 'Dorel Bolovaneanu',
-      'issueId': 'PPDS0003',
-      'title': 'Some Issue Title',
-      'version': '1.200.2',
-      'asignee': 'Bogdan Popescu',
-      'priority': 'REJECTED',
-      'status': 'MEDIUM',
-      'createdOn': '21 Feb 2022',
-      'description': 'Dummy Description',
-      'reproducingSteps': 'Steps to reproduce the issue',
-      'asignedOn': '21 Feb 2022',
-      'environment': 'Some Environment',
-      'closedBy': 'Ionut Predeal',
-      'closedOn': '12 Mar 2022',
-      'tester': 'Bogdan Popescu',
-      'discussion': ['comment', 'another comment'],
-      'project': 'Project'
-    },
-    {
-      'createdBy': 'Dorel Bolovaneanu',
-      'issueId': 'PPDS0004',
-      'title': 'Some Issue Title',
-      'version': '1.200.2',
-      'asignee': 'Ionut Predeal',
-      'priority': 'HIGH',
-      'status': 'OPEN',
-      'createdOn': '21 Feb 2022',
-      'description': 'Dummy Description',
-      'reproducingSteps': 'Steps to reproduce the issue',
-      'asignedOn': '21 Feb 2022',
-      'environment': 'Some Environment',
-      'closedBy': 'Ionut Predeal',
-      'closedOn': '12 Mar 2022',
-      'tester': 'Bogdan Popescu',
-      'discussion': ['comment', 'another comment'],
-      'project': 'Project'
+  private issuesUrl = `${environment.apiUrl}/issues`;
+
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService
+    ) { }
+
+  getIssues(): Observable<IssueModel[]> {
+    return this.http.get<IssueModel[]>(this.issuesUrl)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  getIssue(issueId: string): Observable<IssueModel> {
+    return this.http.get<IssueModel>(`${this.issuesUrl}/${issueId}`);
+  }
+
+  createIssue(
+    issueRequest: IssueRequestModel,
+    projectId: string
+    ): Observable<void> {
+    return this.http.post<void>(
+      `${this.issuesUrl}/createOnProject/${projectId}`,
+      issueRequest
+    );
+  }
+
+  changeIssueStatus(issueId: string, status: IssueStatusRequest): Observable<void> {
+    let changeStateRequest = `${this.issuesUrl}/${issueId}/${status}`;
+    let request = changeStateRequest;
+
+    if(status === IssueStatusRequest.assignToDeveloper || status === IssueStatusRequest.closeByDeveloper) {
+      let developerId = this.authService.getCurrentUser().userId;
+      request = `${request}/${developerId}`;
     }
-  ];
 
-  constructor() { }
+    return this.http.put<void>(
+      request,
+      null
+    );
+  }
 
-  getIssues(): Observable<any[]> {
-    return of(this.issues);
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 }
