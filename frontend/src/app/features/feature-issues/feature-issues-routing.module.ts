22b95952a92f25bdbc50552862f954d548cd7b67
AlexBosca <alex_241998@yahoo.com>
2023-03-15 06:14:38 +0200

    frontend - created issues feature

--- frontend/src/app/features/feature-issues/feature-issues-routing.module.ts
+++ frontend/src/app/features/feature-issues/feature-issues-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IssueDetailsComponent } from './components/issue-details/issue-details.component';
+import { PageIssueDetailsComponent } from './pages/page-issue-details/page-issue-details.component';
 import { PageIssuesComponent } from './pages/page-issues/page-issues.component';
 
 const routes: Routes = [
+  {
+    path: 'details/:id',
+    component: PageIssueDetailsComponent,
+  },
   {
     path: '',
     component: PageIssuesComponent
--- frontend/src/app/features/feature-issues/feature-issues.module.ts
+++ frontend/src/app/features/feature-issues/feature-issues.module.ts
@@ -1,21 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { FeatureIssuesRoutingModule } from './feature-issues-routing.module';
 import { IssuesTableComponent } from './components/issues-table/issues-table.component';
 import { PageIssuesComponent } from './pages/page-issues/page-issues.component';
 import { IssuesFilterComponent } from './components/issues-filter/issues-filter.component';
+import { IssueDetailsComponent } from './components/issue-details/issue-details.component';
+import { PageIssueDetailsComponent } from './pages/page-issue-details/page-issue-details.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BasicAuthInterceptor } from 'src/app/core/interceptor/basic-auth.interceptor';
 
 
 @NgModule({
   declarations: [
     IssuesTableComponent,
     PageIssuesComponent,
-    IssuesFilterComponent
+    IssuesFilterComponent,
+    IssueDetailsComponent,
+    PageIssueDetailsComponent
   ],
   imports: [
     CommonModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     FeatureIssuesRoutingModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS, useClass: BasicAuthInterceptor, multi: true
+    }
   ]
 })
 export class FeatureIssuesModule { }
