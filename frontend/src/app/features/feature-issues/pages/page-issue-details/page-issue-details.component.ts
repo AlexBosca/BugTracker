a65958f26d9311b89bf25845a0cd04fc25e0e706
AlexBosca <alex_241998@yahoo.com>
2023-03-15 06:27:52 +0200

    frontend - created issue details page

---
+++ frontend/src/app/features/feature-issues/pages/page-issue-details/page-issue-details.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './page-issue-details.component.html',
+  styleUrls: ['./page-issue-details.component.css']
+})
+export class PageIssueDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
