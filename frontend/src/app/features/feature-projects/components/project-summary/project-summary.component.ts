831593910b3f141136446714921c9355a09ca5cf
AlexBosca <alex_241998@yahoo.com>
2023-05-18 22:20:37 +0300

    CU-85zruzxe4 - Implement Project Summary Page

--- frontend/src/app/features/feature-projects/components/project-summary/project-summary.component.ts
+++ frontend/src/app/features/feature-projects/components/project-summary/project-summary.component.ts
@@ -3,7 +3,10 @@ import { ProjectModel } from '../../models/ProjectModel';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { ProjectService } from '../../services/project.service';
 import { HttpErrorResponse } from '@angular/common/http';
-import { ChartData, ChartEvent, ChartType, ChartConfiguration } from 'chart.js';
+import { ChartData, ChartEvent, ChartType, ChartConfiguration, Chart } from 'chart.js';
+import { IssueModel } from 'src/app/features/feature-issues/models/IssueModel';
+import { Status } from 'src/app/features/feature-issues/models/status.enum';
+import { IssueStatusRequest } from 'src/app/features/feature-issues/models/IssueStatusRequest';
 
 @Component({
   selector: 'app-project-summary',
@@ -13,32 +16,15 @@ import { ChartData, ChartEvent, ChartType, ChartConfiguration } from 'chart.js';
 export class ProjectSummaryComponent implements OnInit {
   private projectId!: string| null;
   project!: ProjectModel;
+  issues: IssueModel[] = [];
+  doneIssues: IssueModel[] = [];
+  updatedIssues: IssueModel[] = [];
+  createdIssues: IssueModel[] = [];
+  dueIssues: IssueModel[] = [];
+  todoIssues: IssueModel[] = [];
+  inProgressIssues: IssueModel[] =[];
   error!: HttpErrorResponse;
-  // Doughnut Chart
-  public doughnutChartLabels: string[] = ['Done', 'To do', 'In progress'];
-  public doughnutChartData: ChartData<'doughnut'> = {
-    labels: this.doughnutChartLabels,
-    datasets: [
-      {
-        label: 'Dataset 1',
-        data: [1, 2, 3],
-        backgroundColor: [
-          '#14A44D',
-          '#9FA6B2',
-          '#3B71CA'
-        ]
-      }
-    ]
-  };
-  
-  public doughnutChartOptions: ChartConfiguration<'doughnut'>['options'] = {
-    plugins: {
-      legend: {
-        position: 'right'
-      }
-    }
-  }
-  public doughnutChartType: ChartType = 'doughnut';
+  issueStatusOverviewChart: any;
 
   // Bar Chart
   public barChartLegend = false;
@@ -79,6 +65,7 @@ export class ProjectSummaryComponent implements OnInit {
     );
 
     this.fetchProject();
+    this.fetchIssuesOnProject();
   }
 
   fetchProject(): void {
@@ -86,6 +73,45 @@ export class ProjectSummaryComponent implements OnInit {
       this.projectService.getProject(this.projectId)
         .subscribe({
           next: data => this.project = data,
+          error: error => this.error = error
+        });
+    }
+  }
+
+  fetchIssuesOnProject(): void {
+    if(this.projectId) {
+      this.projectService.getIssuesOnProject(this.projectId)
+        .subscribe({
+          next: data => {
+            this.issues = data;
+            this.doneIssues = data.filter(issue => [Status.VERIFIED, Status.CLOSED, Status.DEFERRED, Status.DUPLICATE, Status.REJECTED, Status.NOT_A_BUG].includes(issue.status));    // verify DONE states
+            this.todoIssues = data.filter(issue => [Status.NEW, Status.ASSIGNED, Status.OPEN, Status.REOPENED].includes(issue.status));    // verify TODO states
+            this.inProgressIssues = data.filter(issue => [Status.PENDING_RETEST, Status.RETEST, Status.FIXED].includes(issue.status));    // verify IN PROGRESS states
+
+            this.issueStatusOverviewChart = new Chart(
+              'issueStatusOverviewChart',
+              {
+                type: 'doughnut',
+                data: {
+                  labels: ['Done', 'To do', 'In progress'],
+                  datasets: [
+                    {
+                      label: '',
+                      data: [this.doneIssues.length, this.todoIssues.length, this.inProgressIssues.length],
+                      backgroundColor: ['#14A44D','#9FA6B2','#3B71CA']
+                    }
+                  ],
+                },
+                options: {
+                  plugins: {
+                    legend: {
+                      position: 'right'
+                    }
+                  }
+                }
+              }
+            );
+          },
           error: error => this.error = error
         });
     }
