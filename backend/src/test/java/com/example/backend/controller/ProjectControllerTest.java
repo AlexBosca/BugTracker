02e236f63b18f72e2270bacd96748eb5a44ba1e6
AlexBosca <alex_241998@yahoo.com>
2023-07-03 20:39:10 +0300

    CU-85zt2ge0n - Solve java:S5786 code smell in ProjectControllerTest.java

--- backend/src/test/java/com/example/backend/controller/ProjectControllerTest.java
+++ backend/src/test/java/com/example/backend/controller/ProjectControllerTest.java
@@ -1,22 +1,16 @@
 package com.example.backend.controller;
 
-import com.example.backend.dto.request.IssueRequest;
 import com.example.backend.dto.response.IssueFullResponse;
 import com.example.backend.dto.response.ProjectFullResponse;
 import com.example.backend.entity.ProjectEntity;
-import com.example.backend.entity.UserEntity;
 import com.example.backend.entity.issue.IssueEntity;
 import com.example.backend.exception.project.ProjectNotFoundException;
 import com.example.backend.mapper.MapStructMapper;
-import com.example.backend.service.IssueService;
 import com.example.backend.service.ProjectService;
-import com.example.backend.service.AuthenticationService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -24,7 +18,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.security.core.Authentication;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
@@ -33,18 +26,12 @@ import java.time.Clock;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import static com.example.backend.enums.IssuePriority.LOW;
-import static com.example.backend.enums.IssueStatus.NEW;
 
 import static com.example.backend.util.ExceptionUtilities.PROJECT_WITH_ID_NOT_FOUND;
-import static com.example.backend.util.ExceptionUtilities.PROJECT_ALREADY_CREATED;
-import static com.example.backend.util.ExceptionUtilities.TEAM_WITH_ID_NOT_FOUND;
 
 // @Disabled
 @Profile("test")
@@ -53,7 +40,7 @@ import static com.example.backend.util.ExceptionUtilities.TEAM_WITH_ID_NOT_FOUND
 @WebMvcTest(controllers = ProjectController.class, useDefaultFilters = false, excludeAutoConfiguration = SecurityAutoConfiguration.class)
 @Import(ProjectController.class)
 @ComponentScan({"com.example.backend.mapper"})
-public class ProjectControllerTest {
+class ProjectControllerTest {
 
     @Autowired
     private MapStructMapper mapStructMapper;
@@ -72,7 +59,7 @@ public class ProjectControllerTest {
 
     @Test
     @DisplayName("Should return OK status and a not empty list when there are projects")
-    public void shouldGetAllProjectsAndOkStatusWhenThereAreProjects() throws Exception {
+    void shouldGetAllProjectsAndOkStatusWhenThereAreProjects() throws Exception {
         ProjectEntity firstExpectedProject = ProjectEntity.builder()
             .projectKey("PROJECT1")
             .name("First Project")
@@ -108,7 +95,7 @@ public class ProjectControllerTest {
 
     @Test
     @DisplayName("Should return OK status and an empty list when there are no projects")
-    public void shouldGetAllProjectsAndOkStatusWhenThereAreNoProjects() throws Exception {
+    void shouldGetAllProjectsAndOkStatusWhenThereAreNoProjects() throws Exception {
         List<ProjectEntity> expectedProjects = List.of();
 
         when(projectService.getAllProjects()).thenReturn(expectedProjects);
@@ -123,7 +110,7 @@ public class ProjectControllerTest {
 
     @Test
     @DisplayName("Should return OK status and an empty list when there are no projects")
-    public void shouldGetProjectByProjectKeyAndOkStatusWhenProjectExists() throws Exception {
+    void shouldGetProjectByProjectKeyAndOkStatusWhenProjectExists() throws Exception {
         String givenProjectKey = "PROJECT1";
 
         ProjectEntity expectedProject = ProjectEntity.builder()
@@ -144,7 +131,7 @@ public class ProjectControllerTest {
 
     @Test
     @DisplayName("Should return OK status and a not empty list when there are issues on project by given projectKey")
-    public void shouldGetAllIssuesOnProjectByProjectKeyAndOkStatusWhenProjectExistsAndThereAreIssues() throws Exception {
+    void shouldGetAllIssuesOnProjectByProjectKeyAndOkStatusWhenProjectExistsAndThereAreIssues() throws Exception {
         String givenProjectKey = "PROJECT1";
 
         IssueEntity firstExpectedIssue = IssueEntity.builder()
@@ -190,7 +177,7 @@ public class ProjectControllerTest {
 
     @Test
     @DisplayName("Should return OK status and an empty list when there are no issues on project by given projectKey")
-    public void shouldGetAllIssuesOnProjectByProjectKeyAndOkStatusWhenProjectExistsAndThereAreNoIssues() throws Exception {
+    void shouldGetAllIssuesOnProjectByProjectKeyAndOkStatusWhenProjectExistsAndThereAreNoIssues() throws Exception {
         String givenProjectKey = "PROJECT1";
 
         List<IssueEntity> expectedIssues = List.of();
@@ -207,7 +194,7 @@ public class ProjectControllerTest {
 
     @Test
     @DisplayName("Should return NOT_FOUND status and throw ProjectNotFoundException when the project to return the issues created on does not exist")
-    public void shouldThrowProjectNotFoundExceptionndNotFoundStatusWhenProjectDoesNotExist() throws Exception {
+    void shouldThrowProjectNotFoundExceptionndNotFoundStatusWhenProjectDoesNotExist() throws Exception {
         String givenProjectKey = "PROJECT1";
 
         when(projectService.getAllIssuesOnProjectById(givenProjectKey)).thenThrow(new ProjectNotFoundException(givenProjectKey));
