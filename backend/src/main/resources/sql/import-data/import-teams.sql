e087f4062b904b23ab592be4e48490574cceea69
AlexBosca <alex_241998@yahoo.com>
2023-07-24 21:55:43 +0300

    CU-85zt335z1 - Create development DB

---
+++ backend/src/main/resources/sql/import-data/import-teams.sql
@@ -0,0 +1,5 @@
+INSERT INTO teams (team_id, name) VALUES ('DevS', 'Development Sharks');
+INSERT INTO teams (team_id, name) VALUES ('TDol', 'Testing Dolphins');
+INSERT INTO teams (team_id, name) VALUES ('DevC', 'Development Capybaras');
+INSERT INTO teams (team_id, name) VALUES ('TCapy', 'Testing Cpybaras');
+INSERT INTO teams (team_id, name) VALUES ('DevW', 'Development Weirdos');
