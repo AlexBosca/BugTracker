7f6de5e32d6ae2e34a2c6628aa8d9c3a78341044
AlexBosca <alex_241998@yahoo.com>
2024-04-16 00:19:00 +0300

    CU-86bxxy4m7 - Develop Backend Logic for Project Creation Deleted import-teams-on-project.sql, import-teams.sql and import-users-in-teams.sql Removed all team dependencies from schema.sql Added new annotations to ProjectEntity

--- backend/src/main/resources/sql/import-data/import-teams-on-project.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO teams_on_project (project_id, team_id) VALUES (1, 1);
-INSERT INTO teams_on_project (project_id, team_id) VALUES (1, 2);
-INSERT INTO teams_on_project (project_id, team_id) VALUES (2, 3);
-INSERT INTO teams_on_project (project_id, team_id) VALUES (2, 4);
-INSERT INTO teams_on_project (project_id, team_id) VALUES (3, 5);
--- backend/src/main/resources/sql/import-data/import-users-in-teams.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users_in_team (team_id, user_id) VALUES (1, 1);
-INSERT INTO users_in_team (team_id, user_id) VALUES (1, 2);
-INSERT INTO users_in_team (team_id, user_id) VALUES (1, 3);
-INSERT INTO users_in_team (team_id, user_id) VALUES (1, 4);
-INSERT INTO users_in_team (team_id, user_id) VALUES (1, 5);
