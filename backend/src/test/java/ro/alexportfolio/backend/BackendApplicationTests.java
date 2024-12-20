fa8dc8dfe5467eda0297f8e5fd747c13f0697ae0
AlexBosca <alex_241998@yahoo.com>
2024-12-20 23:28:50 +0200

    CU-86c0w3y9r - Test commit

--- backend/src/test/java/ro/alexportfolio/backend/BackendApplicationTests.java
+++ backend/src/test/java/ro/alexportfolio/backend/BackendApplicationTests.java
@@ -3,11 +3,15 @@ package ro.alexportfolio.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 import ro.alexportfolio.backend.dao.IssueRepository;
 import ro.alexportfolio.backend.dao.ProjectRepository;
 import ro.alexportfolio.backend.dao.UserRepository;
 
+@Profile("test")
+@ActiveProfiles("test")
 @SpringBootTest
 class BackendApplicationTests {
 
