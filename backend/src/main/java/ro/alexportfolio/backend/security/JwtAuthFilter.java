884ffd34b27446dcdec545caf50cb52ff5814cd5
AlexBosca <alex_241998@yahoo.com>
2025-09-28 16:11:17 +0300

    CU-86bxvk86m - Removed printStackTrace call

--- backend/src/main/java/ro/alexportfolio/backend/security/JwtAuthFilter.java
+++ backend/src/main/java/ro/alexportfolio/backend/security/JwtAuthFilter.java
@@ -53,7 +53,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             } catch (ExpiredJwtException e) {
 
             } catch (Exception e) {
-                e.printStackTrace();
+
             }
         }
 
