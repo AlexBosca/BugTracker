c8b6c21ac9c912661f5e0d309bee3b515fc73880
AlexBosca <alex_241998@yahoo.com>
2026-01-13 22:10:35 +0200

    CU-86c63rg0h - Removed suppression for test

--- backend/src/main/java/ro/alexportfolio/backend/BackendApplication.java
+++ backend/src/main/java/ro/alexportfolio/backend/BackendApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "ro.alexportfolio.backend")
-@SuppressWarnings("java:S1118") // Sonar: Utility class should not have public constructor
 public class BackendApplication {
 
 	public static void main(String[] args) {
